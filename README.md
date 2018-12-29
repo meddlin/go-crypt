55121c0963e754119e47c6ee45dbbc483be49f34
meddlin <drushing.dev@gmail.com>
2018-12-29 14:20:33 -0600

    It's presentable now.
    - Basic ROT-5 default (works with "home row")
    - Diagnostics put into playByPlay() function

---
+++ README.md
@@ -0,0 +1,10 @@
+## go-crypt
+
+A simple ROT-style cipher written in Go to learn the language.
+
+### Getting Started
+
+- Clone
+- Navigate to directory
+- `go build`
+- `go-crypt`
--- hello.go
+++ hello.go
@@ -18,10 +18,15 @@ func main() {
 	fmt.Print("Encrypt/Decrypt (E / d)? : ")
 	option, _ := reader.ReadString('\n')
 
-	fmt.Println("Seed entered: " + text)
+	fmt.Print("Seed entered: " + text)
 	fmt.Println("Option: " + option)
 
-	encrypt(text)
+	if string(option[0]) == "E" || string(option[0]) == "e" {
+		encrypt(text)
+	}
+	if string(option[0]) == "D" || string(option[0]) == "d" {
+		decrypt(text)
+	}
 }
 
 // Key concepts : Iterate over a string with a basic loop
@@ -30,14 +35,35 @@ func encrypt(in_str string) {
 
 	// The (- 2) is a cheap way to bypass the \r\n tacked onto the end of the input
 	for i := 0; i < (len(in_str) - 2); i++ {
-		encodedVal := in_str[i] * 2
-
-		fmt.Printf("%v", i)
-		fmt.Printf("%v\n", ": " + string(in_str[i]) + " => " + string(encodedVal))
-
+		encodedVal := in_str[i] + 5
 		encodedString += string(encodedVal)
+
+		// Uncomment to see how each byte moves in the ROT-13 process
+		//playByPlay(i, in_str, encodedVal)
 	}
 
-	fmt.Printf("%v\n", "Encoded!")
+	fmt.Printf("%v\n", "Encrypted!")
 	fmt.Println(encodedString)
+}
+
+// Decrypt function performs the "mathematical reverse" of the encrypt function
+func decrypt(in_str string) {
+	decodedString := ""
+
+	for i := 0; i < (len(in_str) - 2); i++ {
+		decodedVal := in_str[i] - 5			// Too large of an offset will break the current ASCII cipher
+		decodedString += string(decodedVal)
+
+		// Uncomment to see how each byte moves in the ROT-13 process
+		//playByPlay(i, in_str, decodedVal)
+	}
+
+	fmt.Printf("%v\n", "Decrypted!")
+	fmt.Println(decodedString)
+}
+
+// Displays the "diagnostics" of the cipher movement
+func playByPlay(counter int, in_str string, val byte) {
+	fmt.Printf("%v", counter)
+	fmt.Printf("%v\n", ": " + string(in_str[counter]) + " => " + string(val))
 }
